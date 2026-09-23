6f35875645c56a275eb3d5efd92ca889ababb86b
Sibuchanda <sibuchanda457@gmail.com>
2026-09-23 09:40:00 +0530

    Chain: simple chain

---
+++ Chains/simpleChain.py
@@ -0,0 +1,20 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+parser = StrOutputParser()
+
+model = ChatGoogleGenerativeAI(model="gemini-3.7-flash")
+
+# Instantiation using from_template (recommended)
+prompt = PromptTemplate.from_template("What is the capital of : {topic}")
+# prompt.format(topic="India")
+
+chain = prompt | model | parser
+res = chain.invoke({'topic':'India'})
+print(res)
+
+chain.get_graph().print_ascii()
+
